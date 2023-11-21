11f58156ab50b37679df129bcc6bdcb8b29d6386
Lewas88 <Lemaprieto@uma.es>
2023-11-21 14:14:58 +0100

    Respawn and Animation

--- Assets/Scripts/FruitManager.cs
+++ Assets/Scripts/FruitManager.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class FruitManager : MonoBehaviour
 {
+    private void Update(){
+        AllFruitsCollected();
+    }
+    public Text levelCleared;
     public void AllFruitsCollected(){
         if (transform.childCount==0)
         {
             Debug.Log("No quedan frutas, Victoria");
+            levelCleared.gameObject.SetActive(true);
+            //Invoke("ChangeScene",1);
         }
+    }
+
+    void ChangeScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 }
