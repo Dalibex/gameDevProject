db941f3f7b2f52e7bf44c8a84b491c320c85c697
DanielLBUMA <dalibexyt@gmail.com>
2023-12-12 17:03:30 +0100

    Comments
    Comentarios al código de mainMenu y settingsMenu

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/* Para las escenas usamos la libreria de SceneManagement que viene con
+"SceneManager." */
 public class MainMenu : MonoBehaviour
 {
+    //Cambiar escena de pantalla de menu principal al juego (SOLO FUNCIONA PARA ESTE CASO!!)
     public void PlayGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex); 
     }
 
+    //Cerrar el juego
     public void QuitGame() {
         Application.Quit();
     }
