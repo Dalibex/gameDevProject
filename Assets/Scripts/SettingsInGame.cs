476479c1ed829cfb9226c1a36c80390f5f35a6ee
Lewas88 <Lemaprieto@uma.es>
2025-01-13 19:30:41 +0100

    UI

--- Assets/Scripts/SettingsInGame.cs
+++ Assets/Scripts/SettingsInGame.cs
@@ -12,11 +12,17 @@ public class SettingsInGame : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Escape)) {
             aj =! aj;
+            Time.timeScale = aj ? 0 : 1;
         }
         options.SetActive(aj);
     }
 
     public void close() {
         aj = false;
+        Time.timeScale = 1;
+    }
+    public void returnMainMenu() {
+        Time.timeScale = 1;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 }
